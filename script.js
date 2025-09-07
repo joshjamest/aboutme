14eb0fd3e44e1375b3d184847c086ac43970e288
Joshua Treffiletti <joshua.j.treffiletti@gmail.com>
2025-09-07 23:03:48 +1000

    dropdowns

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// to do
+const headers = document.querySelectorAll(".accordion-header");
+
+headers.forEach(header => {
+  header.addEventListener("click", () => {
+    const content = header.nextElementSibling;
+    const openItem = document.querySelector(".accordion-content[style*='max-height']");
+    if (openItem && openItem !== content) {
+      openItem.style.maxHeight = null;
+    }
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+});
